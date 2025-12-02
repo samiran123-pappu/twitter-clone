891fd6bae6a6d62efc5cc817015a2ffe2d58a2a4
Samiran Sarkar <pappu@gmail.com>
2025-12-02 13:22:29 +0530

    1st commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+    res.json({data: "signup"});
+}   
+export const login = (req, res) => {
+    res.json({data: "login"});
+}   
+export const logout = (req, res) => {
+    res.json({data: "logout"});
+}
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+console.log(process.env.MONGO_URI);
+
+
+app.use('/api/auth', authRoutes);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+  connectMongoDB();
+});
+
+
+
