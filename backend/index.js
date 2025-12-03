b8ccaf43a7c9a0a5a1211ab2347487f0a8d20261
Samiran Sarkar <pappu@gmail.com>
2025-12-03 15:17:50 +0530

    add userRoutes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
 import dotenv from 'dotenv';
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from 'cookie-parser';
+import {v2 as cloudinary} from 'cloudinary';
+
+
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 
@@ -18,6 +28,7 @@ app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-enco
 
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 
 
