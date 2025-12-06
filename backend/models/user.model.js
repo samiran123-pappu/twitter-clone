da052a0a588040d969877c04bccf194457af30a2
Samiran Sarkar <pappu@gmail.com>
2025-12-06 19:52:20 +0530

    yes commit

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
 
-
-const userSchema = new mongoose.Schema({
-    	username: {
+const userSchema = new mongoose.Schema(
+	{
+		username: {
 			type: String,
 			required: true,
 			unique: true,
 		},
-        fullname: {
-            type: String,
-            required: true, 
-        },
-        password: {
-            type: String,
-            required: true,
-            minLength: 6,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+		fullName: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 6,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
 		followers: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
 				default: [],
 			},
 		],
-        profileImg: {
+		profileImg: {
 			type: String,
 			default: "",
 		},
@@ -52,12 +52,17 @@ const userSchema = new mongoose.Schema({
 			type: String,
 			default: "",
 		},
-        
-}, {timestamps: true});
+		likedPosts: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Post",
+				default: [],
+			},
+		],
+	},
+	{ timestamps: true }
+);
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
-
-
-
-
